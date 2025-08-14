27f6fa3e09639d2af2c69d3ac463f21601d35c11
dev <idevendrajput@gmail.com>
2025-08-15 01:43:26 +0530

    Fix CORS: Add support for admin panel ports 3002 and 3040

--- server.js
+++ server.js
@@ -17,8 +17,12 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:3001', 
+    'http://localhost:3002',
+    'http://localhost:3040',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
+    'http://127.0.0.1:3002',
+    'http://127.0.0.1:3040',
     'https://trustfolio.in',
     'https://api.trustfolio.in',
     'https://admin.trustfolio.in'
